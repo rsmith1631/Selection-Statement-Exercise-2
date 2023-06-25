2fc15bd74a7e59a881802d499eeba5a9965b25ba
rsmith1631 <rsmith1631@gmail.com>
2023-06-25 14:10:39 -0700

    Favorite Subject options has been added

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        Console.WriteLine("What is your favorite school subject");
+        string userSubject = Console.ReadLine();
+
+        switch(userSubject.ToLower()) 
+        {
+            case "math":
+                Console.WriteLine($"Math is a great subject");
+                break;
+            case "english":
+                Console.WriteLine($"English is where it all begins");
+                break;
+            case "social studies":
+                Console.WriteLine($"Social Studies is how we understand one another!");
+                break;
+            case "science":
+                Console.WriteLine($"Science is where innovations happen!");
+                break;
+            case "spanish":
+                Console.WriteLine($"It's always good to know a second language!");
+                break;
+            default:
+                Console.WriteLine($"I wish you the best on your continued studies!");
+                break;
+        }
         }
     }
 }
