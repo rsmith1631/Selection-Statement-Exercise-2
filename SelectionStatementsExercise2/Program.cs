b45fa5fc1db848059b1d3c8523043d1d6cfe0580
rsmith1631 <rsmith1631@gmail.com>
2023-06-27 11:01:41 -0700

    Created a new project for switch case

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Security.Cryptography.X509Certificates;
+
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("To get started... What is your favorite subject?");
+
+            var studentAnswer = Console.ReadLine();
+
+            switch (studentAnswer.ToLower()) 
+            {
+                case "Electrical Engineer":
+                    Console.WriteLine($"Amazing! That's a great career field.");
+                    break;
+                case "Software Engineer":
+                    Console.WriteLine($"You're on your to do great things!");
+                    break;
+                case "Painter":
+                    Console.WriteLine($"What a way to tap into your creativity");
+                    break;
+                case "Trainer":
+                    Console.WriteLine($"You will be the reason most of the world will stay fit");
+                    break;
+                case "Pilot":
+                    Console.WriteLine($"One of the best ways to travel the world. You must be a genius?!");
+                    break;
+                default:
+                    Console.WriteLine($"I know you will change the world one day! Wish you the best in all of your endeavors!");
+                    break;
+                
+            }
         }
     }
 }
